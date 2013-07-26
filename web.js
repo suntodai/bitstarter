a15c7b68bea30da741e27085e8387cca875cb753
suntodai <sunchaohitgmail.com>
2013-07-26 01:07:47 +0000

    modify web.js with index.html

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-var fs=require('fs');
-var buf = new Buffer(256);
 
-buf.write(fs.readFileSync('index.html','utf-8'));
-var content = buf.toString('utf8',0,buf);
-console.log("write: "+content);
 app.get('/', function(request, response) {
+
+  var fs=require('fs');
+  var buf = new Buffer(256);
+  buf.write(fs.readFileSync('index.html','utf-8'));
+  var content = buf.toString('utf8');
   response.send(content);
 });
 
