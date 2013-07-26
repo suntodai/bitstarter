665d30b1f8369250a73ee115881aadc2c084eac2
suntodai <sunchaohitgmail.com>
2013-07-26 00:50:27 +0000

    update web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs=require('fs');
+var buf = new Buffer(256);
 
+buf.write(fs.readFileSync('index.html','utf-8'));
+var content = buf.toString('utf8',0,buf);
+console.log("write: "+content);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
