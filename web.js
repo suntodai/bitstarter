7f388cc718ed55eb1ca95789966f89e5656333d6
suntodai <sunchaohit@gmail.com>
2013-08-02 14:32:14 +0000

    modify web.js to support img

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+app.use(express.static(__dirname + '/assets'));
 
   var fs=require('fs');
   buf = new Buffer(fs.readFileSync('index.html','utf-8'));
